b10ea602c0392774f1547e1d32477c3b7a7342f9
Stephane Dzefo <fredicm25@gmail.com>
2019-12-19 16:56:33 +0100

    first commit

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,8 @@
+<?php 
+//version anglaise
+return [
+    'slogan' => 'Discover, Share and live unique experience near you.',
+    'cookie_consentement'=>'  By continuing to browse this site, you consent to our use of cookies to improve you online experience .',
+    'learn_more'=>'learn more'
+
+];
---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,8 @@
+<?php
+//version francaise
+
+return [
+    'slogan' => 'Découvrez, partagez et vivez une expérience unique près de chez vous.',
+    'cookie_consentement'=>' En poursuivant votre navigation sur ce site, vous consentez à notre utilisation des cookies pour améliorer votre expérience en ligne.',
+    'learn_more'=>'En savoir plus'
+];
